0b521648e79c54a73a7d00ba388ac46222df8b95
Eduard-Stefan <126601093+Eduard-Stefan@users.noreply.github.com>
2023-03-13 20:48:39 +0200

    Delete AbstractClassExample directory

--- AbstractClassExample/src/Car.java
+++
@@ -1,8 +0,0 @@
-class Car extends Vehicle {
-    public Car(int wheels, int doors) {
-        super(wheels, doors);
-    }
-    public void start() {
-        System.out.println("The car is starting...");
-    }
-}
