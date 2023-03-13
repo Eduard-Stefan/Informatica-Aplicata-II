dbd2cf8beaf6523b24503becda55f98367643546
Eduard-Stefan <126601093+Eduard-Stefan@users.noreply.github.com>
2023-03-13 20:58:30 +0200

    Delete Homework 1.3 directory

--- Homework 1.3/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person person = new Person("Cojocaru Eduard-Stefan", 20);
-        System.out.println(person.toString());
-    }
-}
